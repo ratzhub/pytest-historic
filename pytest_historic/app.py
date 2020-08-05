ab265f7497407ac493f4ec9eefe8832ec1554c06
Rateesh Manoharan <rateesh.manoharan@atherenergy.com>
2020-08-05 18:37:01 +0530

    Update for Static Code Analysis

--- pytest_historic/app.py
+++ pytest_historic/app.py
@@ -1,7 +1,21 @@
+import codecs
+import csv
+import json
+import os
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 import config
 from .args import parse_options
+from werkzeug.utils import secure_filename
+import traceback
+
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger()
+logger.addHandler(logging.FileHandler('test.log', 'a'))
+print = logger.info
 
 app = Flask(__name__, template_folder='templates')
 
@@ -20,16 +34,35 @@ def redirect_url():
 
 @app.route('/home', methods=['GET'])
 def home():
+    return render_template('home.html')
+
+
+@app.route('/automation-home', methods=['GET'])
+def automation_home():
     cursor = mysql.connection.cursor()
     use_db(cursor, "pytesthistoric")
     cursor.execute("select * from TB_PROJECT;")
     data = cursor.fetchall()
-    return render_template('home.html', data=data)
+    return render_template('automation-home.html', data=data)
+
+
+@app.route('/sa-home', methods=['GET'])
+def sa_home():
+    cursor = mysql.connection.cursor()
+    use_db(cursor, "pytesthistoric")
+    cursor.execute("select * from SA_PROJECT;")
+    data = cursor.fetchall()
+    return render_template('sa-home.html', data=data)
 
 
 @app.route('/<db>/deldbconf', methods=['GET'])
 def delete_db_conf(db):
     return render_template('deldbconf.html', db_name=db)
+
+
+@app.route('/<db>/sa-deldbconf', methods=['GET'])
+def sa_delete_db_conf(db):
+    return render_template('sa-deldbconf.html', db_name=db)
 
 
 @app.route('/<db>/delete', methods=['GET'])
@@ -42,6 +75,16 @@ def delete_db(db):
     return redirect(url_for('home'))
 
 
+@app.route('/<db>/sa-delete', methods=['GET'])
+def sa_delete_db(db):
+    cursor = mysql.connection.cursor()
+    cursor.execute("DROP DATABASE %s;" % db)
+    # use_db(cursor, "pytesthistoric")
+    cursor.execute("DELETE FROM pytesthistoric.SA_PROJECT WHERE Project_Name='%s';" % db)
+    mysql.connection.commit()
+    return redirect(url_for('sa_home'))
+
+
 @app.route('/newdb', methods=['GET', 'POST'])
 def add_db():
     if request.method == "POST":
@@ -50,14 +93,13 @@ def add_db():
         db_image = request.form['dbimage']
         db_webhook = request.form['dbwebhook']
         cursor = mysql.connection.cursor()
-
         try:
             # create new database for project
             cursor.execute("Create DATABASE %s;" % db_name)
             # update created database info in pytesthistoric.TB_PROJECT table
             cursor.execute(
                 "INSERT INTO pytesthistoric.TB_PROJECT ( Project_Id, Project_Name, Project_Desc, Project_Image, Created_Date, Last_Updated, Total_Executions, Recent_Pass_Perc, Overall_Pass_Perc, Project_Webhook) VALUES (0, '%s', '%s', '%s', NOW(), NOW(), 0, 0, 0, '%s');" % (
-                db_name, db_desc, db_image, db_webhook))
+                    db_name, db_desc, db_image, db_webhook))
             # create tables in created database
             use_db(cursor, db_name)
             cursor.execute(
@@ -68,12 +110,44 @@ def add_db():
                 "Create table TB_TEST ( Test_Id INT NOT NULL auto_increment primary key, Execution_Id INT, Test_Name TEXT, Test_Status CHAR(5), Test_Time FLOAT, Test_Error TEXT, Test_Comment TEXT);")
             mysql.connection.commit()
         except Exception as e:
-            print(str(e))
+            print(traceback.format_exc())
 
         finally:
             return redirect(url_for('home'))
     else:
         return render_template('newdb.html')
+
+
+@app.route('/sa-newdb', methods=['GET', 'POST'])
+def sa_add_db():
+    if request.method == "POST":
+        db_name = request.form['dbname']
+        db_desc = request.form['dbdesc']
+        db_image = request.form['dbimage']
+        db_webhook = request.form['dbwebhook']
+        cursor = mysql.connection.cursor()
+        try:
+            # create new database for project
+            cursor.execute("Create DATABASE %s;" % db_name)
+            # update created database info in pytesthistoric.SA_PROJECT table
+            cursor.execute(
+                "INSERT INTO pytesthistoric.SA_PROJECT ( Project_Id, Project_Name, Project_Desc, Project_Image, Created_Date, Last_Updated, Total_Executions, Project_Webhook) VALUES (0, '%s', '%s', '%s', NOW(), NOW(), 0, '%s');" % (
+                    db_name, db_desc, db_image, db_webhook))
+            # create tables in created database
+            use_db(cursor, db_name)
+            print("static_analysis")
+            cursor.execute(
+                "Create table SA_EXECUTION ( Execution_Id INT NOT NULL auto_increment primary key, Execution_Date DATETIME, Component_Version TEXT, Build_Version TEXT, Pipeline_Link TEXT, Artifact_Link TEXT, Priority_High INT, Priority_Low INT, Priority_Medium INT);")
+            cursor.execute(
+                "Create table SA_DEFECT ( Defect_Id INT NOT NULL auto_increment primary key, Execution_Id INT, Defect_Category TEXT, Defect_Check TEXT, Defect_Priority TEXT, Defect_File_Path TEXT, Defect_Function TEXT, Defect_Begin_Line INT, Defect_End_Line INT, Defect_Column INT, Defect_Comment TEXT);")
+            mysql.connection.commit()
+        except Exception as e:
+            print(traceback.format_exc())
+
+        finally:
+            return redirect(url_for('sa_home'))
+    else:
+        return render_template('sa-newdb.html')
 
 
 @app.route('/<db_name>/editdb', methods=['GET', 'POST'])
@@ -106,6 +180,38 @@ def edit_db(db_name):
             return redirect(url_for('home'))
     else:
         return render_template('editdb.html', db_name=db_name)
+
+
+@app.route('/<db_name>/sa-editdb', methods=['GET', 'POST'])
+def sa_edit_db(db_name):
+    if request.method == "POST":
+        db_desc = request.form['dbdesc']
+        db_image = request.form['dbimage']
+        db_webhook = request.form['dbwebhook']
+        cursor = mysql.connection.cursor()
+
+        try:
+            # update created database info in pytesthistoric.TB_PROJECT table
+            if db_desc:
+                cursor.execute(
+                    "UPDATE pytesthistoric.SA_PROJECT SET Project_Desc = '%s' WHERE Project_Name = '%s';" % (
+                        db_desc, db_name))
+            if db_image:
+                cursor.execute(
+                    "UPDATE pytesthistoric.SA_PROJECT SET Project_Image ='%s' WHERE Project_Name = '%s';" % (
+                        db_image, db_name))
+            if db_webhook:
+                cursor.execute(
+                    "UPDATE pytesthistoric.SA_PROJECT SET Project_Webhook='%s' WHERE Project_Name = '%s';" % (
+                        db_webhook, db_name))
+            mysql.connection.commit()
+        except Exception as e:
+            print(str(e))
+
+        finally:
+            return redirect(url_for('sa-home'))
+    else:
+        return render_template('sa-editdb.html', db_name=db_name)
 
 
 @app.route('/<db>/dashboard', methods=['GET'])
@@ -162,6 +268,58 @@ def dashboard(db):
         return redirect(url_for('redirect_url'))
 
 
+@app.route('/<db>/sa-dashboard', methods=['GET'])
+def sa_dashboard(db):
+    cursor = mysql.connection.cursor()
+    use_db(cursor, db)
+
+    cursor.execute("SELECT COUNT(Execution_Id) from SA_EXECUTION;")
+    results_data = cursor.fetchall()
+    cursor.execute("SELECT COUNT(Defect_Id) from SA_DEFECT;")
+    defects_data = cursor.fetchall()
+
+    if results_data[0][0] > 0 and defects_data[0][0] > 0:
+
+        cursor.execute(
+            "SELECT Priority_High, Priority_Low, Priority_Medium from SA_EXECUTION order by Execution_Id desc LIMIT 1;")
+        last_exe_pie_data = cursor.fetchall()
+
+        cursor.execute(
+            "SELECT SUM(Priority_High), SUM(Priority_Low), SUM(Priority_Medium), COUNT(Execution_Id) from (SELECT Priority_High, Priority_Low, Priority_Medium, Execution_Id from SA_EXECUTION order by Execution_Id desc LIMIT 10) as T;")
+        last_ten_exe_pie_data = cursor.fetchall()
+
+        cursor.execute(
+            "SELECT SUM(Priority_High), SUM(Priority_Low), SUM(Priority_Medium), COUNT(Execution_Id) from SA_EXECUTION order by Execution_Id desc;")
+        over_all_exe_pie_data = cursor.fetchall()
+
+        cursor.execute(
+            "SELECT Component_Version, Priority_High, Priority_Low, Priority_Medium from SA_EXECUTION order by Execution_Id desc LIMIT 10;")
+        last_ten_data = cursor.fetchall()
+
+        cursor.execute(
+            "SELECT ROUND(MIN(Priority_High),2), ROUND(AVG(Priority_High),2), ROUND(MAX(Priority_High),2) from SA_EXECUTION order by Execution_Id desc;")
+        high_priority_data = cursor.fetchall()
+
+        cursor.execute(
+            "SELECT ROUND(MIN(Priority_Low),2), ROUND(AVG(Priority_Low),2), ROUND(MAX(Priority_Low),2) from SA_EXECUTION order by Execution_Id desc;")
+        low_priority_data = cursor.fetchall()
+
+        cursor.execute(
+            "SELECT ROUND(MIN(Priority_Medium),2), ROUND(AVG(Priority_Medium),2), ROUND(MAX(Priority_Medium),2) from SA_EXECUTION order by Execution_Id desc;")
+        medium_priority_data = cursor.fetchall()
+
+        return render_template('sa-dashboard.html', last_ten_data=last_ten_data,
+                               last_exe_pie_data=last_exe_pie_data,
+                               last_ten_exe_pie_data=last_ten_exe_pie_data,
+                               over_all_exe_pie_data=over_all_exe_pie_data,
+                               high_priority_data=high_priority_data,
+                               low_priority_data=low_priority_data,
+                               medium_priority_data=medium_priority_data, db_name=db)
+
+    else:
+        return redirect(url_for('redirect_url'))
+
+
 @app.route('/<db>/ehistoric', methods=['GET'])
 def ehistoric(db):
     cursor = mysql.connection.cursor()
@@ -171,9 +329,23 @@ def ehistoric(db):
     return render_template('ehistoric.html', data=data, db_name=db)
 
 
+@app.route('/<db>/sa-ehistoric', methods=['GET'])
+def sa_ehistoric(db):
+    cursor = mysql.connection.cursor()
+    use_db(cursor, db)
+    cursor.execute("SELECT * from SA_EXECUTION order by Execution_Id desc LIMIT 500;")
+    data = cursor.fetchall()
+    return render_template('sa-ehistoric.html', data=data, db_name=db)
+
+
 @app.route('/<db>/deleconf/<eid>', methods=['GET'])
 def delete_eid_conf(db, eid):
     return render_template('deleconf.html', db_name=db, eid=eid)
+
+
+@app.route('/<db>/sa-deleconf/<eid>', methods=['GET'])
+def sa_delete_eid_conf(db, eid):
+    return render_template('sa-deleconf.html', db_name=db, eid=eid)
 
 
 @app.route('/<db>/edelete/<eid>', methods=['GET'])
@@ -202,10 +374,30 @@ def delete_eid(db, eid):
     # update pytesthistoric project
     cursor.execute(
         "UPDATE pytesthistoric.TB_PROJECT SET Total_Executions=%s, Last_Updated=now(), Recent_Pass_Perc=%s WHERE Project_Name='%s';" % (
-        int(exe_data[0][0]), recent_pass_perf, db))
+            int(exe_data[0][0]), recent_pass_perf, db))
     # commit changes
     mysql.connection.commit()
     return redirect(url_for('ehistoric', db=db))
+
+
+@app.route('/<db>/sa-edelete/<eid>', methods=['GET'])
+def sa_delete_eid(db, eid):
+    cursor = mysql.connection.cursor()
+    use_db(cursor, db)
+    # remove execution from tables: execution, suite, test
+    cursor.execute("DELETE FROM SA_EXECUTION WHERE Execution_Id='%s';" % eid)
+    cursor.execute("DELETE FROM SA_DEFECT WHERE Execution_Id='%s';" % eid)
+    # get no. of executions
+    cursor.execute("SELECT COUNT(*) from SA_EXECUTION;")
+    exe_data = cursor.fetchall()
+
+    # update pytesthistoric project
+    cursor.execute(
+        "UPDATE pytesthistoric.SA_PROJECT SET Total_Executions=%s, Last_Updated=now() WHERE Project_Name='%s';" % (
+            int(exe_data[0][0]), db))
+    # commit changes
+    mysql.connection.commit()
+    return redirect(url_for('sa_ehistoric', db=db))
 
 
 @app.route('/<db>/tmetrics', methods=['GET', 'POST'])
@@ -225,6 +417,25 @@ def tmetrics(db):
     cursor.execute("SELECT * from TB_TEST WHERE Execution_Id=%s;" % data)
     data = cursor.fetchall()
     return render_template('tmetrics.html', data=data, db_name=db)
+
+
+@app.route('/<db>/sa-dmetrics', methods=['GET', 'POST'])
+def sa_dmetrics(db):
+    cursor = mysql.connection.cursor()
+    use_db(cursor, db)
+    if request.method == "POST":
+        textField = request.form['textField']
+        rowField = request.form['rowField']
+        cursor.execute("Update SA_DEFECT SET Defect_Comment='%s' WHERE Defect_Id=%s;" % (str(textField), str(rowField)))
+        mysql.connection.commit()
+
+    # Get last row execution ID
+    cursor.execute("SELECT Execution_Id from SA_EXECUTION order by Execution_Id desc LIMIT 1;")
+    data = cursor.fetchone()
+    # Get testcase results of execution id (typically last executed)
+    cursor.execute("SELECT * from SA_DEFECT WHERE Execution_Id=%s;" % data)
+    data = cursor.fetchall()
+    return render_template('sa-dmetrics.html', data=data, db_name=db)
 
 
 @app.route('/<db>/metrics/<eid>', methods=['GET'])
@@ -247,6 +458,34 @@ def metrics(db, eid):
                            project_image=project_image[0][0], exe_data=exe_data)
 
 
+@app.route('/<db>/sa-metrics/<eid>', methods=['GET'])
+def sa_metrics(db, eid):
+    cursor = mysql.connection.cursor()
+    use_db(cursor, db)
+    # Get testcase results of execution id
+    cursor.execute("SELECT * from SA_DEFECT WHERE Execution_Id=%s;" % eid)
+    test_data = cursor.fetchall()
+    # get project image
+    cursor.execute("SELECT Project_Image from pytesthistoric.SA_PROJECT WHERE Project_Name='%s';" % db)
+    project_image = cursor.fetchall()
+    # get execution info
+    cursor.execute("SELECT * from SA_EXECUTION WHERE Execution_Id=%s;" % eid)
+    exe_data = cursor.fetchall()
+    # get defect check info
+    cursor.execute(f"SELECT COUNT(Defect_Id), Defect_Check from SA_DEFECT WHERE Execution_Id = {eid} "
+                   f"GROUP BY Defect_Check ORDER BY COUNT(Defect_Id) DESC;")
+    defect_check_data = cursor.fetchall()
+    # get defect category info
+    cursor.execute(f"SELECT COUNT(Defect_Id), Defect_Category from SA_DEFECT WHERE Execution_Id = {eid} "
+                   f"GROUP BY Defect_Category ORDER BY COUNT(Defect_Id) DESC;")
+    defect_category_data = cursor.fetchall()
+    graph_data = {"check": defect_check_data, "category": defect_category_data}
+    graph_data = json.dumps(graph_data)
+    return render_template('sa-metrics.html', test_data=test_data,
+                           project_image=project_image[0][0], exe_data=exe_data,
+                           graph_data=graph_data)
+
+
 @app.route('/<db>/tmetrics/<eid>', methods=['GET', 'POST'])
 def eid_tmetrics(db, eid):
     cursor = mysql.connection.cursor()
@@ -261,6 +500,22 @@ def eid_tmetrics(db, eid):
     cursor.execute("SELECT * from TB_TEST WHERE Execution_Id=%s;" % eid)
     data = cursor.fetchall()
     return render_template('eidtmetrics.html', data=data, db_name=db)
+
+
+@app.route('/<db>/sa-dmetrics/<eid>', methods=['GET', 'POST'])
+def sa_eid_tmetrics(db, eid):
+    cursor = mysql.connection.cursor()
+    use_db(cursor, db)
+    if request.method == "POST":
+        textField = request.form['textField']
+        rowField = request.form['rowField']
+        cursor.execute("Update SA_DEFECT SET Defect_Comment='%s' WHERE Defect_Id=%s;" % (str(textField), str(rowField)))
+        mysql.connection.commit()
+
+    # Get testcase results of execution id (typically last executed)
+    cursor.execute("SELECT * from SA_DEFECT WHERE Execution_Id=%s;" % eid)
+    data = cursor.fetchall()
+    return render_template('sa-eiddmetrics.html', data=data, db_name=db)
 
 
 @app.route('/<db>/failures/<eid>', methods=['GET', 'POST'])
@@ -339,6 +594,74 @@ def compare(db):
         return render_template('compare.html', db_name=db)
 
 
+def parse_sa_report(csv_file, tool, cursor, eid):
+    defect_count = 0
+    if tool == "polyspace":
+        csv_reader = csv.reader(codecs.iterdecode(csv_file, 'utf-8'), delimiter='\t')
+        next(csv_reader)  # Skipping header row
+        for row in csv_reader:
+            cursor.execute(
+                f"INSERT INTO SA_DEFECT (Execution_Id, Defect_Category, Defect_Check, Defect_Priority, Defect_File_Path, Defect_Function)"
+                f" VALUES ({eid}, '{row[2]}', '{row[5]}', '{row[6].strip('Impact: ')}','{row[9]}', '{row[8]}');")
+            defect_count += 1
+    # Update priority counts:
+    temp_count = {"high": [],
+                  "low": [],
+                  "mediu": []}
+    for priority in temp_count:
+        cursor.execute(
+            f"SELECT COUNT(Defect_Id) FROM SA_DEFECT WHERE Defect_Priority = '{priority}' AND  Execution_id = {eid};")
+        count = cursor.fetchall()
+        temp_count[priority] = count[0][0]
+    command = f"UPDATE SA_EXECUTION SET Priority_High = {temp_count['high']}, Priority_Low = {temp_count['low']}, " \
+              f"Priority_Medium = {temp_count['mediu']} WHERE Execution_Id = {eid};"
+    print(command)
+    cursor.execute(command)
+    return defect_count
+
+
+"""
+curl -X POST "http://10.240.0.87:5000/static" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "file=@/home/rateesh/static_analysis/bcm_app.txt;type=text/plain" -F "artifact-link=http://google.com" -F "pipeline-link=http://facebook.com" -F "tool=polyspace" -F "component=bcmapp" -F "component-version=1.6.3"
+"""
+
+
+@app.route('/static', methods=['POST'])
+def static_report():
+    print(request.form)
+    try:
+        if request.method == 'POST':
+            file = request.files['file']
+            component = request.form['component']
+            component_version = request.form['component-version']
+            build_version = request.form['build-version']
+            artifact_link = request.form['artifact-link']
+            pipeline_link = request.form['pipeline-link']
+            commit_id = request.form['commit-id']
+            git_branch = request.form['git-branch']
+            repo_link = request.form['repo-link']
+            project_dir = request.form['project-dir']
+            tool = request.form['tool']
+            cursor = mysql.connection.cursor()
+            use_db(cursor, component)
+            cursor.execute(
+                f"INSERT INTO SA_EXECUTION (Execution_Date, Component_Version, Pipeline_Link, Artifact_Link, Build_Version) "
+                f"VALUES (NOW(), '{component_version}', '{pipeline_link}', '{artifact_link}', '{build_version}');")
+            mysql.connection.commit()
+            cursor.execute(
+                "SELECT Execution_Id FROM SA_EXECUTION ORDER BY Execution_Id DESC LIMIT 1;")
+            rows = cursor.fetchone()
+            eid = rows[0]
+            defect_count = parse_sa_report(file, tool, cursor, eid)
+            cursor.execute(
+                f"UPDATE pytesthistoric.SA_PROJECT SET Total_Executions=Total_Executions+1 WHERE Project_Name='{component}';")
+            mysql.connection.commit()
+
+            return {"Defects_added_to_db": defect_count}
+    except Exception as e:
+        print(traceback.format_exc())
+        return {"Exception": traceback.format_exc()}
+
+
 def use_db(cursor, db_name):
     cursor.execute("USE %s;" % db_name)
 
@@ -354,6 +677,8 @@ def sort_tests(data_list):
 
 
 def main():
+
+
     args = parse_options()
 
     app.config['MYSQL_HOST'] = args.sqlhost
